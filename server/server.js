e9c7a3c513dab936bca4f60867f9cd8062b13085
Syed <saghirashar@gmail.com>
2021-06-07 17:31:36 +0500

    comit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const io = require('socket.io')(5000)
 io.on('connection', socket => {
   const id = socket.handshake.query.id
   socket.join(id)
-
+  console.log('connected');
   socket.on('send-message', ({ recipients, text }) => {
     recipients.forEach(recipient => {
       const newRecipients = recipients.filter(r => r !== recipient)
